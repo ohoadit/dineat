85976a1ccf49293c0a8607c7c1033e8b12976d50
feedbackloop08 <aditshah8@gmail.com>
2020-04-12 12:32:20 +0530

    visual changes, made it look alike login card

--- client/src/views/Reset.vue
+++ client/src/views/Reset.vue
@@ -1,54 +1,59 @@
 <template>
   <v-app>
-    <v-row justify="center" class="mt-10"> 
-      <v-col cols="10" xs="10" sm="8" md="6" class="mt-10">
-      <v-card tile elevation="5">
-        <v-toolbar color="secondary white--text">
-          <v-toolbar-title class="headline">
-            Dineat
-          </v-toolbar-title>
-        </v-toolbar>
-        <v-card-title class="subtitle font-weight-regular"
-          >Set your Password
-        </v-card-title>
-        <v-card-text class="px-10">
-          <v-form ref="form" @submit.prevent="onSubmit">
-            <v-text-field
-              autofocus
-              v-model="password"
-              label="New password"
-              :value="password"
-              type="password"
-              name="password"
-              :rules="[rules.isEmpty, rules.checkLen]"
-            >
-            </v-text-field>
-            <v-text-field
-              v-model="confirm"
-              label="Confirm password"
-              :value="confirm"
-              type="password"
-              name="confirm"
-              :rules="[rules.isEmpty, rules.checkLen, match]"
-            >
-            </v-text-field>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn
-                color="primary"
-                type="submit"
-                tile
-                elevation="3"
-                width="100px"
-                :disabled="disabled"
-                >Submit</v-btn
-              >
-            </v-card-actions>
-          </v-form>
-        </v-card-text>
-      </v-card>
-      </v-col>
-    </v-row>
+    <v-container fluid>
+      <v-row class="mt-10">
+        <v-col cols="12">
+          <p class="display-1 font-weight-regular" align="center">Dineat</p>
+          <v-row class="mt-10 px-2" justify="center">
+            <v-card tile elevation="5" width="500px">
+              <v-row justify="center">
+                <v-card-title class="title font-weight-regular"
+                  >Set your Password
+                </v-card-title>
+              </v-row>
+              <v-card-text class="px-7">
+                <v-form ref="form" @submit.prevent="onSubmit">
+                  <v-text-field
+                    autofocus
+                    v-model="password"
+                    label="New password"
+                    :value="password"
+                    type="password"
+                    name="password"
+                    :rules="[rules.isEmpty, rules.checkLen]"
+                    validate-on-blur
+                  >
+                  </v-text-field>
+                  <v-text-field
+                    v-model="confirm"
+                    label="Confirm password"
+                    :value="confirm"
+                    type="password"
+                    name="confirm"
+                    :rules="[rules.isEmpty, rules.checkLen, match]"
+                    validate-on-blur
+                  >
+                  </v-text-field>
+                  <v-card-actions>
+                    <v-row justify="center" class="mt-5">
+                      <v-btn
+                        color="primary"
+                        type="submit"
+                        tile
+                        elevation="3"
+                        width="100px"
+                        :disabled="disabled"
+                        >Submit</v-btn
+                      >
+                    </v-row>
+                  </v-card-actions>
+                </v-form>
+              </v-card-text>
+            </v-card>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
     <v-snackbar v-model="snackbar" :color="color" :timeout="timeout">{{
       message
     }}</v-snackbar>
@@ -95,11 +100,11 @@ export default {
         const reply = await res.json();
         this.message = reply.msg;
         if (reply.valid) {
-            this.color = "teal accent-4";
-            this.snackbar = true;
-          } else {
-            this.color = "red lighten-1";
-            this.snackbar = true;
+          this.color = "teal accent-4";
+          this.snackbar = true;
+        } else {
+          this.color = "red lighten-1";
+          this.snackbar = true;
         }
         setTimeout(() => {
           this.$router.push("/login");
