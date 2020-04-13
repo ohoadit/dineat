32c133fbd5a297914707d2e5b05a58d10872365b
feedbackloop08 <aditshah8@gmail.com>
2020-04-13 23:15:29 +0530

    register router import change

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const compression = require("compression");
 const app = express();
 
 const loginRouter = require("./routes/login");
-const registerRouter = require("./routes/register");
+const { registerRouter } = require("./routes/register");
 const dashboardRouter = require("./routes/dashboard");
 const masterRouter = require("./routes/master");
 
@@ -28,7 +28,7 @@ app
 app.use(express.static(__dirname + "/client/dist/"));
 app.get("/*", (req, res) => {
   res.sendFile(__dirname + "/client/dist/index.html", (err) => {
-    res.status(500).send(err)
+    res.status(500).send(err);
   });
 });
 
